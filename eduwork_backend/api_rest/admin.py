68a12d81934eaa832ba2725ee9d5cd09d9bc7747
Danny <danysadiaz4@gmail.com>
2026-08-01 16:41:26 -0700

    Actualización de modelos de datos

--- eduwork_backend/api_rest/admin.py
+++ eduwork_backend/api_rest/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(State)
+admin.site.register(CompanySector)
+admin.site.register(City)
+admin.site.register(JobType)
+admin.site.register(Degree)
+admin.site.register(Skill)
+admin.site.register(StudentProfile)
+admin.site.register(CompanyProfile)
+admin.site.register(University)
+admin.site.register(Career)
+admin.site.register(Job)
+admin.site.register(Application)
+admin.site.register(Interview)
+admin.site.register(StudentSkill)
+admin.site.register(JobSkill)
+admin.site.register(SavedJob)
