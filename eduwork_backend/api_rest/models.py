f5ee4d5e4df85f0908478a07b50b4ad3adef5a1b
Danny <danysadiaz4@gmail.com>
2026-08-02 16:19:08 -0700

    Reimplementación de campos Enums

--- eduwork_backend/api_rest/models.py
+++ eduwork_backend/api_rest/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django_enum import EnumField
 
 # Create your models here.
 
@@ -21,53 +22,51 @@ class JobType(models.Model):
             return self.description
 
 class Degree(models.Model):
-    DEGREE_TYPE = [
-        ('D', 'DOCTORATE'),
-        ('M', 'MASTER'),
-        ('P', 'POSTGRADUATE'),
-        ('E', 'ENGINEERING'),
-        ('B', 'BACHELOR'),
-        ('T', 'TECHNICAL'),
-    ]
+    class DegreeType(models.TextChoices):
+        DOCTORATE = 'D', 'DOCTORATE'
+        MASTER = 'M', 'MASTER'
+        POSTGRADUATE = 'P', 'POSTGRADUATE'
+        ENGINEERING = 'E', 'ENGINEERING'
+        BACHELOR = 'B', 'BACHELOR'
+        TECHNICAL = 'T', 'TECHNICAL'
     name = models.CharField(max_length=60, null=False, blank=False, unique=True)
-    type = models.CharField(max_length=1, choices=DEGREE_TYPE, null=False, blank=False)
+    type = EnumField(DegreeType, null=False, blank=False)
 
     def __str__(self) -> str:
         return self.name
 
 class Skill(models.Model):
-    SKILL_TYPE = [
-        ('S', 'SOFT'),
-        ('H', 'HARD'),
-        ('L', 'LANGUAGE')
-    ]
+    class SkillType(models.TextChoices):
+        SOFT = 'S', 'SOFT'
+        HARD = 'H', 'HARD'
+        LANGUAGE = 'L', 'LANGUAGE'
     name = models.CharField(max_length=60, null=False, blank=False, unique=True)
-    type = models.CharField(max_length=1, choices=SKILL_TYPE, null=False, blank=False)
+    type = EnumField(SkillType, null=False, blank=False)
 
     def __str__(self) -> str:
         return self.name
 
 class City(models.Model):
     name = models.CharField(max_length=60, null=False, blank=False, unique=True)
-    state_id = models.ForeignKey(State, on_delete=models.RESTRICT, null=False, blank=False)
+    state = models.ForeignKey(State, on_delete=models.RESTRICT, null=False, blank=False)
 
     def __str__(self) -> str:
             return self.name
 
 class StudentProfile(models.Model):
-    GENDER_TYPE = [
-        ('M', 'MALE'),
-        ('F', 'FEMALE'),
-        ('N', 'NON_BINARY'),
-    ]
+    class GenderType(models.TextChoices):
+        MALE = 'M', 'MALE'
+        FEMALE = 'F', 'FEMALE'
+        NON_BINARY = 'N', 'NON_BINARY'
+
     first_name = models.CharField(max_length=60, null=False, blank=False)
     last_name = models.CharField(max_length=60, null=False, blank=False)
     biografy = models.TextField(max_length=500, null=True, blank=True)
-    gender = models.CharField(max_length=1, choices=GENDER_TYPE, null=False, blank=False)
+    gender = EnumField(GenderType, null=False, blank=False)
     date_of_birth = models.DateField(null=False, blank=False)
     email_address = models.CharField(max_length=60, null=False, blank=False, unique=True)
     phone_number = models.CharField(max_length=20, null=False, blank=False, unique=True)
-    city_id = models.ForeignKey(City, on_delete=models.CASCADE, null=False, blank=False)
+    city = models.ForeignKey(City, on_delete=models.CASCADE, null=False, blank=False)
     registered_on = models.DateTimeField(auto_now_add=True, null=False, blank=False)
 
     def __str__(self) -> str:
@@ -76,7 +75,7 @@ class StudentProfile(models.Model):
 class CompanyProfile(models.Model):
     name = models.CharField(max_length=60, null=False, blank=False)
     biography = models.TextField(max_length=500, null=True, blank=True)
-    sector_id = models.ForeignKey(CompanySector, on_delete=models.CASCADE, null=False, blank=False)
+    sector = models.ForeignKey(CompanySector, on_delete=models.CASCADE, null=False, blank=False)
     email_address = models.CharField(max_length=60, null=False, blank=False, unique=True)
     phone_number = models.CharField(max_length=20, null=False, blank=False, unique=True)
     address = models.CharField(max_length=200, null=True, blank=True)
@@ -96,23 +95,21 @@ class University(models.Model):
             return self.name
 
 class Career(models.Model):
-    STATUS_TYPE = [
-        ('I', 'INTERNSHIP'),
-        ('C', 'COMPLETED'),
-        ('G', 'GRADUATED'),
-        ('D', 'DEGREE_IN_PROGRESS'),
-        ('U', 'INCOMPLETE'),
-    ]
-    TERM_TYPE = [
-        ('S', 'SEMESTER'),
-        ('F', 'FOUR_MONTH_TERM'),
-        ('O', 'OPEN'),
-    ]
-    student_id = models.ForeignKey(StudentProfile, on_delete=models.CASCADE, null=False, blank=False)
-    university_id = models.ForeignKey(University, on_delete=models.CASCADE, null=False, blank=False)
-    degree_id = models.ForeignKey(Degree, on_delete=models.CASCADE, null=False, blank=False)
-    status = models.CharField(max_length=1, choices=STATUS_TYPE, null=False, blank=False)
-    term_type = models.CharField(max_length=1, choices=TERM_TYPE, null=False, blank=False)
+    class StatusType(models.TextChoices):
+        INTERNSHIP = 'I', 'INTERNSHIP'
+        COMPLETED = 'C', 'COMPLETED'
+        GRADUATED = 'G', 'GRADUATED'
+        DEGREE_IN_PROGRESS = 'D', 'DEGREE_IN_PROGRESS'
+        INCOMPLETE = 'U', 'INCOMPLETE'
+    class TermType(models.TextChoices):
+        SEMESTER = 'S', 'SEMESTER'
+        FOUR_MONTH_TERM = 'F', 'FOUR_MONTH_TERM'
+        OPEN = 'O', 'OPEN'
+    student = models.ForeignKey(StudentProfile, on_delete=models.CASCADE, null=False, blank=False)
+    university = models.ForeignKey(University, on_delete=models.CASCADE, null=False, blank=False)
+    degree = models.ForeignKey(Degree, on_delete=models.CASCADE, null=False, blank=False)
+    status = EnumField(StatusType, null=False, blank=False)
+    term_type = EnumField(TermType, null=False, blank=False)
     starting_date = models.DateField(null=False, blank=False)
     finishing_date = models.DateField(null=True, blank=True)
 
@@ -120,28 +117,26 @@ class Career(models.Model):
             return f"{self.starting_date}-{self.finishing_date}"
 
 class Job(models.Model):
-    SALARY_PERIOD_TYPE = [
-        ('H', 'HOURLY'),
-        ('D', 'DAILY'),
-        ('W', 'WEEKLY'),
-        ('M', 'MONTHLY'),
-        ('Y', 'YEARLY'),
-        ('P', 'PER_PROJECT'),
-    ]
-    WORKPLACE_TYPE = [
-        ('O', 'ON_SITE'),
-        ('H', 'HYBRID'),
-        ('R', 'REMOTE'),
-    ]
+    class SalaryPeriodType(models.TextChoices):
+        HOURLY = 'H', 'HOURLY'
+        DAILY = 'D', 'DAILY'
+        WEEKLY = 'W', 'WEEKLY'
+        MONTHLY = 'M', 'MONTHLY'
+        YEARLY = 'Y', 'YEARLY'
+        PER_PROJECT = 'P', 'PER_PROJECT'
+    class WorkplaceType(models.TextChoices):
+        ON_SITE = 'O', 'ON_SITE'
+        HYBRID = 'H', 'HYBRID'
+        REMOTE = 'R', 'REMOTE'
     title = models.CharField(max_length=200, null=False, blank=False)
-    company_id = models.ForeignKey(CompanyProfile, on_delete=models.CASCADE, null=False, blank=False)
+    company = models.ForeignKey(CompanyProfile, on_delete=models.CASCADE, null=False, blank=False)
     min_salary = models.DecimalField(max_digits=8, decimal_places=3, null=False, blank=False)
     max_salary = models.DecimalField(max_digits=8, decimal_places=3, null=False, blank=False)
-    salary_period = models.CharField(max_length=1, choices=SALARY_PERIOD_TYPE, null=False, blank=False)
-    workplace_type = models.CharField(max_length=1, choices=WORKPLACE_TYPE, null=False, blank=False)
-    degree_id = models.ForeignKey(Degree, on_delete=models.CASCADE, null=False, blank=False)
-    type_id = models.ForeignKey(JobType, on_delete=models.CASCADE, null=False, blank=False)
-    city_id = models.ForeignKey(City, on_delete=models.CASCADE, null=False, blank=False)
+    salary_period = EnumField(SalaryPeriodType, null=False, blank=False)
+    workplace_type = EnumField(WorkplaceType, null=False, blank=False)
+    degree = models.ForeignKey(Degree, on_delete=models.CASCADE, null=False, blank=False)
+    type = models.ForeignKey(JobType, on_delete=models.CASCADE, null=False, blank=False)
+    city = models.ForeignKey(City, on_delete=models.CASCADE, null=False, blank=False)
     is_active = models.BooleanField(default=True, null=False, blank=False)
     published_on = models.DateTimeField(auto_now_add=True, null=False, blank=False)
 
@@ -149,18 +144,17 @@ class Job(models.Model):
             return f"{self.title} ({self.published_on})"
 
 class Application(models.Model):
-    STATUS_TYPE = [
-        ('A', 'APPLIED'),
-        ('U', 'UNDER_REVIEW'),
-        ('S', 'SHORTLISTED'),
-        ('P', 'IN_PROGRESS'),
-        ('H', 'HIRED'),
-        ('R', 'REJECTED'),
-        ('W', 'WITHDRAWN'),
-    ]
-    student_id = models.ForeignKey(StudentProfile, on_delete=models.CASCADE, null=False, blank=False)
-    job_id = models.ForeignKey(Job, on_delete=models.CASCADE, null=False, blank=False)
-    status = models.CharField(max_length=1, choices=STATUS_TYPE, null=False, blank=False)
+    class StatusType(models.TextChoices):
+        APPLIED = 'A', 'APPLIED'
+        UNDER_REVIEW = 'U', 'UNDER_REVIEW'
+        SHORTLISTED = 'S', 'SHORTLISTED'
+        IN_PROGRESS = 'P', 'IN_PROGRESS'
+        HIRED = 'H', 'HIRED'
+        REJECTED = 'R', 'REJECTED'
+        WITHDRAWN = 'W', 'WITHDRAWN'
+    student = models.ForeignKey(StudentProfile, on_delete=models.CASCADE, null=False, blank=False)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE, null=False, blank=False)
+    status = EnumField(StatusType, null=False, blank=False)
     created_on = models.DateTimeField(auto_now_add=True, null=False, blank=False)
     updated_on = models.DateTimeField(auto_now=True, null=False, blank=False)
 
@@ -168,29 +162,28 @@ class Application(models.Model):
             return f"#{self.pk} ({self.created_on})"
 
 class Interview(models.Model):
-    STATUS_TYPE = [
-        ('S', 'SCHEDULED'),
-        ('P', 'PENDING_CONFIRMATION'),
-        ('R', 'RESCHEDULED'),
-        ('C', 'COMPLETED'),
-        ('X', 'CANCELLED'),
-        ('N', 'NO_SHOW'),
-    ]
-    application_id = models.ForeignKey(Application, on_delete=models.CASCADE, null=False, blank=False)
+    class StatusType(models.TextChoices):
+        SCHEDULED = 'S', 'SCHEDULED'
+        PENDING_CONFIRMATION = 'P', 'PENDING_CONFIRMATION'
+        RESCHEDULED = 'R', 'RESCHEDULED'
+        COMPLETED = 'C', 'COMPLETED'
+        CANCELLED = 'X', 'CANCELLED'
+        NO_SHOW = 'N', 'NO_SHOW'
+    application = models.ForeignKey(Application, on_delete=models.CASCADE, null=False, blank=False)
     scheduled_date = models.DateTimeField(null=False, blank=False)
     address_or_url = models.CharField(max_length=255, null=False, blank=False)
-    status = models.CharField(max_length=1, choices=STATUS_TYPE, null=False, blank=False)
+    status = EnumField(StatusType, null=False, blank=False)
     registered_on = models.DateTimeField(auto_now_add=True, null=False, blank=False)
 
 class StudentSkill(models.Model):
-    student_id = models.ForeignKey(StudentProfile, on_delete=models.CASCADE, null=False, blank=False)
-    skill_id = models.ForeignKey(Skill, on_delete=models.CASCADE, null=False, blank=False)
+    student = models.ForeignKey(StudentProfile, on_delete=models.CASCADE, null=False, blank=False)
+    skill = models.ForeignKey(Skill, on_delete=models.CASCADE, null=False, blank=False)
 
 class JobSkill(models.Model):
-    job_id = models.ForeignKey(Job, on_delete=models.CASCADE, null=False, blank=False)
-    skill_id = models.ForeignKey(Skill, on_delete=models.CASCADE, null=False, blank=False)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE, null=False, blank=False)
+    skill = models.ForeignKey(Skill, on_delete=models.CASCADE, null=False, blank=False)
 
 class SavedJob(models.Model):
-    job_id = models.ForeignKey(Job, on_delete=models.CASCADE, null=False, blank=False)
-    student_id = models.ForeignKey(StudentProfile, on_delete=models.CASCADE, null=False, blank=False)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE, null=False, blank=False)
+    student = models.ForeignKey(StudentProfile, on_delete=models.CASCADE, null=False, blank=False)
     saved_on = models.DateTimeField(auto_now_add=True, null=False, blank=False)
