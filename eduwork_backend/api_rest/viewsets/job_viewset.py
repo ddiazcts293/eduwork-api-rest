b5b9022848254afa3991fcad8987b4ef6bd6a6c8
Danny Diaz <danysadiaz4@gmail.com>
2026-08-06 02:05:24 -0700

    Integración inicial de filtros

--- eduwork_backend/api_rest/viewsets/job_viewset.py
+++ eduwork_backend/api_rest/viewsets/job_viewset.py
@@ -1,7 +1,26 @@
 from rest_framework import viewsets
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 from ..models import Job
 from ..serializers.job_serializer import JobSerializer
 
 class JobViewSet(viewsets.ModelViewSet):
     queryset = Job.objects.all()
     serializer_class = JobSerializer
+
+    # Motores de filtrado
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    # Filtrado exacto
+    filterset_fields = [
+        'company',
+        'salary_period',
+        'workplace_type',
+        'degree',
+        'job_type',
+        'city',
+        'company__sector',
+    ]
+    # Búsqueda de texto
+    search_fields = ['title', 'description', 'company__name']
+    # Ordenamiento
+    ordering_fields = ['min_salary', 'max_salary', 'published_on']
--- eduwork_backend/eduwork_backend/settings.py
+++ eduwork_backend/eduwork_backend/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'drf_yasg',
     'rest_framework_simplejwt.token_blacklist',
+    'django_filters',
 ]
 
 AUTH_USER_MODEL = 'users.EduWorkUser'
