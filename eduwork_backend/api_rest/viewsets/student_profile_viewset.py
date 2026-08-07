9b956d95d0ddf0d502e0b77cbf0007df4af1b895
Danny Diaz <danysadiaz4@gmail.com>
2026-08-06 19:33:28 -0700

    Protección en endpoint de perfiles de estudiantes

--- eduwork_backend/api_rest/viewsets/student_profile_viewset.py
+++ eduwork_backend/api_rest/viewsets/student_profile_viewset.py
@@ -1,8 +1,48 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from ..models import StudentProfile
 from ..serializers.student_profile_serializer import StudentProfileSerializer
+from users.permissions import IsCompanyUser, IsStudentUser, IsOwnerProfile
+from users.models import EduWorkUser
 
 class StudentProfileViewSet(viewsets.ModelViewSet):
+    """
+    Apartado para consultar los perfiles de los estudiantes.
+    Solo las empresas pueden consultar todo el listado de estudiantes.
+    Solo los estudiantes pueden modificar su información
+    """
     queryset = StudentProfile.objects.all()
     serializer_class = StudentProfileSerializer
     http_method_names = ['get', 'put', 'patch', 'head', 'options']
+
+    def get_permissions(self):
+        if self.action == 'list':
+            # Permite consultar los perfiles de estudiantes solo a las empresas
+            self.permission_classes = [IsCompanyUser]
+        elif self.action == 'retrieve':
+            # Permite consultar un perfil específico a usuarios autenticados
+            self.permission_classes = [IsAuthenticated]
+        elif self.action in ['update', 'partial_update']:
+            # Permite modificar perfiles solo a estudiantes
+            self.permission_classes = [IsStudentUser, IsOwnerProfile]
+        else:
+            # Permite realizar otras acciones solo a estudiantes
+            self.permission_classes = [IsStudentUser]
+
+        return [permission() for permission in self.permission_classes]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.role == EduWorkUser.Role.STUDENT:
+            return StudentProfile.objects.filter(user=user)
+
+        if user.role == EduWorkUser.Role.COMPANY:
+            if hasattr(user, 'company_profile'):
+                return StudentProfile.objects.all()
+            return StudentProfile.objects.none()
+
+        if user.role == EduWorkUser.Role.ADMIN:
+            return StudentProfile.objects.all()
+
+        return StudentProfile.objects.none()
--- eduwork_backend/users/permissions.py
+++ eduwork_backend/users/permissions.py
@@ -22,3 +22,38 @@ class IsStudentUser(BasePermission):
                 EduWorkUser.Role.ADMIN
             ] # verifica que el rol sea 'Company' o 'Admin'
         )
+
+class IsOwnerStudent(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        # Permite consultar por cualquier usuario
+        if request.method in ['GET', 'HEAD', 'OPTIONS']:
+            return True
+
+        # Verifica si el objeto tiene una referencia al perfil de estudiante
+        if not hasattr(request.user, 'student_profile'):
+            return False
+
+        # Verifica si el perfil de la empresa coincide con el del usuario
+        return obj.student == request.user.student_profile
+
+class IsOwnerCompany(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        # Permite consultar por cualquier usuario
+        if request.method in ['GET', 'HEAD', 'OPTIONS']:
+            return True
+
+        # Verifica si el objeto tiene una referencia al perfil de la empresa
+        if not hasattr(request.user, 'company_profile'):
+            return False
+
+        # Verifica si el perfil de la empresa coincide con el del usuario
+        return obj.company == request.user.company_profile
+
+class IsOwnerProfile(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        # Permite consultar por cualquier usuario
+        if request.method in ['GET', 'HEAD', 'OPTIONS']:
+            return True
+
+        # Verifica si el usuario del perfil coincide con el del usuario
+        return obj.user == request.user
