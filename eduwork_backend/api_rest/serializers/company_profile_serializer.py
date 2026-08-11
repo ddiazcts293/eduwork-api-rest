07cca0d5a42d1e66e02929b369954d9d7d18fd83
Danny Diaz <danysadiaz4@gmail.com>
2026-08-11 03:33:37 -0700

    Actualización de permisos y datos devueltos en perfiles de empresas

--- eduwork_backend/api_rest/serializers/company_profile_serializer.py
+++ eduwork_backend/api_rest/serializers/company_profile_serializer.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
-from ..models import CompanyProfile
+from rest_framework.validators import UniqueValidator
 from django.utils import timezone
+from ..models import CompanyProfile
 from ..validators import phone_number_validator
+from .city_serializer import CitySerializer
 
 class CompanyProfileBasicSerializer(serializers.ModelSerializer):
     sector_description = serializers.CharField(source='sector.description', read_only=True)
@@ -16,13 +18,34 @@ class CompanyProfileBasicSerializer(serializers.ModelSerializer):
             'sector_description',
         ]
 
-class CompanyProfileSerializer(serializers.ModelSerializer):
+class CompanyProfileReadSerializer(serializers.ModelSerializer):
+    city = CitySerializer(read_only=True)
+
     class Meta:
         model = CompanyProfile
         exclude = ['user']
         read_only_fields = ['id']
+        depth = 1
 
-    phone_number = serializers.CharField(max_length=20, validators=[phone_number_validator])
+class CompanyProfileWriteSerializer(serializers.ModelSerializer):
+    phone_number = serializers.CharField(
+        max_length=20,
+        validators=[
+            phone_number_validator,
+            UniqueValidator(queryset=CompanyProfile.objects.all())
+        ]
+    )
+    email_address = serializers.EmailField(
+        max_length=60,
+        validators=[
+            UniqueValidator(queryset=CompanyProfile.objects.all())
+        ]
+    )
+
+    class Meta:
+        model = CompanyProfile
+        read_only_fields = ['id', 'user']
+        exclude = ['user']
 
     def validate_establish_year(self, value):
         if value > timezone.now().date().year or value < 578:
--- eduwork_backend/api_rest/viewsets/company_profile_viewset.py
+++ eduwork_backend/api_rest/viewsets/company_profile_viewset.py
@@ -1,8 +1,38 @@
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from users.permissions import IsCompanyUser, IsOwnerProfile
 from ..models import CompanyProfile
-from ..serializers.company_profile_serializer import CompanyProfileSerializer
+from ..serializers.company_profile_serializer import (
+    CompanyProfileBasicSerializer,
+    CompanyProfileReadSerializer,
+    CompanyProfileWriteSerializer
+)
 
 class CompanyProfileViewSet(viewsets.ModelViewSet):
+    """
+    Apartado para consultar las empresas registradas.
+    Cualquiera puede consultar, pero solo los propietarios pueden modificar.
+    """
+
     queryset = CompanyProfile.objects.all()
-    serializer_class = CompanyProfileSerializer
     http_method_names = ['get', 'put', 'patch', 'head', 'options']
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return CompanyProfileBasicSerializer
+        if self.action == 'retrieve':
+            return CompanyProfileReadSerializer
+
+        return CompanyProfileWriteSerializer
+
+    def get_permissions(self):
+        if self.action == 'list':
+            self.permission_classes = [AllowAny]
+        elif self.action == 'retrieve':
+            self.permission_classes = [IsAuthenticated]
+        elif self.action in ['update', 'partial_update']:
+            self.permission_classes = [IsCompanyUser, IsOwnerProfile]
+        else:
+            self.permission_classes = [IsCompanyUser]
+
+        return [permission() for permission in self.permission_classes]
