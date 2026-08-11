5a9241781908eaf08a978a10af76d2026643d04d
Danny Diaz <danysadiaz4@gmail.com>
2026-08-11 04:00:20 -0700

    Actualización de permisos y datos devueltos en vacantes guardadas

--- eduwork_backend/api_rest/serializers/saved_job_serializer.py
+++ eduwork_backend/api_rest/serializers/saved_job_serializer.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
 from ..models import SavedJob
+from .job_serializer import JobReadSerializer
 
-class SavedJobSerializer(serializers.ModelSerializer):
+class SavedJobReadSerializer(serializers.ModelSerializer):
+    job = JobReadSerializer(read_only=True)
+
+    class Meta:
+        model = SavedJob
+        fields = ['id', 'job', 'saved_on']
+        depth = 2
+
+class SavedJobWriteSerializer(serializers.ModelSerializer):
     class Meta:
         model = SavedJob
         fields = '__all__'
-        read_only_fields = ['id']
+        read_only_fields = ['id', 'student']
--- eduwork_backend/api_rest/viewsets/saved_job_viewset.py
+++ eduwork_backend/api_rest/viewsets/saved_job_viewset.py
@@ -1,9 +1,36 @@
 from rest_framework import viewsets
 from ..models import SavedJob
-from ..serializers.saved_job_serializer import SavedJobSerializer
-from users.permissions import IsStudentUser
+from ..serializers.saved_job_serializer import SavedJobWriteSerializer, SavedJobReadSerializer
+from users.permissions import IsStudentUser, IsOwnerStudent
 
 class SavedJobViewSet(viewsets.ModelViewSet):
-    queryset = SavedJob.objects.all()
-    serializer_class = SavedJobSerializer
-    permission_classes = [IsStudentUser]
+    http_method_names = ['get', 'post', 'delete']
+
+    def get_serializer_class(self):
+        if self.action in ['list', 'retrieve']:
+            return SavedJobReadSerializer
+
+        return SavedJobWriteSerializer
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            self.permission_classes = [IsStudentUser]
+        elif self.action in ['update', 'partial_update', 'destroy']:
+            self.permission_classes = [IsStudentUser, IsOwnerStudent]
+        else:
+            self.permission_classes = [IsStudentUser]
+
+        return [permission() for permission in self.permission_classes]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if hasattr(user, 'role') and user.role == 'STUDENT':
+            if hasattr(user, 'student_profile'):
+                return SavedJob.objects.filter(student=user.student_profile)
+
+        return SavedJob.objects.none()
+
+    def perform_create(self, serializer):
+        student_profile = self.request.user.student_profile
+        serializer.save(student=student_profile)
